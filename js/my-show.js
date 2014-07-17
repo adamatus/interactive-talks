b71cb5caa494be7c3fe13ecace2b7d88db691747
Adam Riggall <adam@riggall.info>
2014-07-16 23:10:31 -0500

    Add help overlay at initial load

--- js/my-show.js
+++ js/my-show.js
@@ -75,6 +75,9 @@ d3.select(window).on("keydown", function() {
             navigateBack();
         }
         break;
+      case 72: // 'h' key, pop up help
+        $('#helpModal').modal('show');
+        break;
       case 48: { // 0 key, pop up menu
         if (!visible) {
           d3.selectAll("#menu").style("display", "block");
